2682eed43ebcb9eb92fe70df4447b8dfd7add868
Philippe Gras <philippe.gras@cern.ch>
2019-07-23 11:31:11 +0200

    fix of  VBFHToEEG_M125_MLL-0To60_Dalitz_13TeV_amcatnlo datacards

--- bin/MadGraph5_aMCatNLO/cards/production/13TeV/HGG/VBFHToEEG_MLL-0To60_Dalitz_13TeV_amcatnlo/make_datacards.sh
+++ bin/MadGraph5_aMCatNLO/cards/production/13TeV/HGG/VBFHToEEG_MLL-0To60_Dalitz_13TeV_amcatnlo/make_datacards.sh
@@ -1,18 +1,132 @@
-#***********************************************************************
-#                        MadGraph5_aMC@NLO                             *
-#                                                                      *
-#                      run_card.dat aMC@NLO                            *
-#                                                                      *
-#  This file is used to set the parameters of the run.                 *
-#                                                                      *
-#  Some notation/conventions:                                          *
-#                                                                      *
-#   Lines starting with a hash (#) are info or comments                *
-#                                                                      *
-#   mind the format:   value    = variable     ! comment               *
-#***********************************************************************
-#
-#*******************                                                 
+#!/bin/bash
+
+for MH in 120 125 130; do
+
+d=VBFHToEEG_M${MH}_MLL-0To60_Dalitz_13TeV-amcatnlo
+
+mkdir "$d"
+
+cat > $d/vbfH${MH}_NLO_HtoEEGamma_customizecards.dat <<EOF
+set param_card mass 25 ${MH}.
+EOF
+
+cat > $d/vbfH${MH}_NLO_HtoEEGamma_extramodels.dat <<EOF
+HC_NLO_X0_UFO-v1.3.zip
+
+HC_NLO_X0_UFO-lepton_masses_no_lepton_yukawas-v1.3.tar.gz
+
+EOF
+
+cat > $d/vbfH${MH}_NLO_HtoEEGamma_FKS_params.dat <<EOF
+! ==========================================================================
+! This file sets the different technical parameters intrinsic to the
+! FKS program and which controls the behaviour of the code at run
+! time.  The common user should not edit this file and only experts
+! should venture editing these parameters.
+! ==========================================================================
+!
+! ==========================================================================
+! Arbitrary numerical parameters used in the FKS formalism
+! ==========================================================================
+!
+! To be implemented by the FKS authors
+!
+! ==========================================================================
+! Parameters controlling the tests based on the IR poles comparison
+! ==========================================================================
+!
+! This threshold sets the limiting value for the comparison of the
+! relative difference of the IR pole from the OLP and the one computed
+! internaly by MadFKS.  The value below is used for the first PS
+! points to assess the sanity of the computation. A value ten times
+! smaller will be used for the systematic check of IR poles at
+! runtime.
+! Notice that the systematic comparison of IR poles at run time is
+! only performed when the Monte-Carlo over helicity configurations
+! method is not used.  Set this value to '-1.0d0' if you want the
+! check to always pass.
+#IRPoleCheckThreshold
+-1d0
+! Default :: 1.0d-5
+!
+! ==========================================================================
+! OLP (virtuals) behavior at run time
+! ==========================================================================
+!
+!
+! Set the precision required from the OLP code. The IR poles check
+! will be performed at run time with a threshold ten times loser than
+! the value below. When equal to '-1d0' the default value of the OLP
+! is used, and the poles check is disabled at run time
+!
+#PrecisionVirtualAtRunTime
+-1d0
+! Default :: 1.0d-3
+!
+! ==========================================================================
+! Parameters defining the techniques used for the MC integration
+! ==========================================================================
+!
+! This integer sets what is the minimum number of contributing
+! helicities (in a given subrpocess) which is necessary for MadFKS to
+! switch to the Monte-Carlo over helicity configurations method. Set
+! this to '-1' if you want to forbid the use of this method
+! altogether.
+#NHelForMCoverHels
+4
+! Default :: 4
+!
+! This parameter sets for which fraction of the events the virtual
+! matrix elements should be included. When using MINT, during the
+! grid-setup phase, this number will be updated automatically after
+! each iteration depending on the relative MC uncertainties.
+#VirtualFraction
+1.0d0
+! Default :: 1.0d0
+!
+! This parameter sets the minimal fraction of the events for which the
+! virtual matrix elements should be included.
+#MinVirtualFraction
+0.005d0
+! Default :: 0.005d0
+!
+! ==========================================================================
+! End of FKS_params.dat file
+! ==========================================================================
+EOF
+
+cat > $d/vbfH${MH}_NLO_HtoEEGamma_madspin_card.dat <<EOF
+import model HC_NLO_X0_UFO-lepton_masses_no_lepton_yukawas --bypass_check
+
+set ms_dir ./madspingrid
+
+set Nevents_for_max_weigth 250 # number of events for the estimate of the max. weight
+set max_weight_ps_point 400  # number of PS to estimate the maximum for each event
+
+set max_running_process 1
+
+set spinmode=none
+set run_card mmll=0
+set run_card mmllmax=60
+
+define l+ = e+
+define l- = e-
+
+decay x0 > l+ l- a
+
+launch
+EOF
+
+cat > $d/vbfH${MH}_NLO_HtoEEGamma_proc_card.dat <<EOF
+import model loop_sm-no_b_mass
+
+generate p p > h j j \$\$ w+ w- z [QCD]
+
+output vbfH${MH}_NLO_HtoEEGamma -nojpeg
+EOF
+
+cat > $d/vbfH${MH}_NLO_HtoEEGamma_run_card.dat <<EOF
+*******************                                                 
 # Running parameters
 #*******************                                                 
 #
@@ -55,13 +169,13 @@ average = event_norm ! Normalize events to sum or average to the X sect.
 # PDF choice: this automatically fixes also alpha_s(MZ) and its evol.  *
 #***********************************************************************
  lhapdf    = pdlabel   ! PDF set                                     
- $DEFAULT_PDF_SETS    = lhaid     ! if pdlabel=lhapdf, this is the lhapdf number
+ \$DEFAULT_PDF_SETS = lhaid     ! if pdlabel=lhapdf, this is the lhapdf number
 #***********************************************************************
 # Include the NLO Monte Carlo subtr. terms for the following parton    *
 # shower (HERWIG6 | HERWIGPP | PYTHIA6Q | PYTHIA6PT | PYTHIA8)         *
 # WARNING: PYTHIA6PT works only for processes without FSR!!!!          *
 #***********************************************************************
-  PYTHIA8   = parton_shower
+ PYTHIA8   = parton_shower
 #***********************************************************************
 # Renormalization and factorization scales                             *
 # (Default functional form for the non-fixed scales is the sum of      *
@@ -87,12 +201,14 @@ average = event_norm ! Normalize events to sum or average to the X sect.
 # For scale dependence: factor rw_scale_up/down around central scale   *
 # For PDF uncertainty: use LHAPDF with supported set                   *
 #***********************************************************************
-  T       = reweight_scale   ! reweight to get scale dependence
-  0.5     = rw_Rscale_down   ! lower bound for ren scale variations
-  2.0     = rw_Rscale_up     ! upper bound for ren scale variations
-  0.5     = rw_Fscale_down   ! lower bound for fact scale variations
-  2.0     = rw_Fscale_up     ! upper bound for fact scale variations
-  $DEFAULT_PDF_MEMBERS  = reweight_PDF     ! reweight to get PDF uncertainty
+ .true.   = reweight_scale   ! reweight to get scale dependence
+ 0.5     = rw_Rscale_down   ! lower bound for ren scale variations
+ 2.0     = rw_Rscale_up     ! upper bound for ren scale variations
+ 0.5     = rw_Fscale_down   ! lower bound for fact scale variations
+ 2.0     = rw_Fscale_up     ! upper bound for fact scale variations
+ \$DEFAULT_PDF_MEMBERS = reweight_PDF     ! reweight to get PDF uncertainty
+      ! First of the error PDF sets 
+      ! Last of the error PDF sets
 #***********************************************************************
 # Merging - WARNING! Applies merging only at the hard-event level.     *
 # After showering an MLM-type merging should be applied as well.       *
@@ -142,4 +258,6 @@ average = event_norm ! Normalize events to sum or average to the X sect.
 #***********************************************************************
  5 = maxjetflavor
 #***********************************************************************
-T = store_rwgt_info
+EOF
+
+done
